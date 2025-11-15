9432e03b2f732f1d503c850c6aba145b4a4eff59
Zass12345678 <Wwinarni821@gmail.com>
2025-11-16 03:58:44 +0700

    Database

---
+++ config.js
@@ -0,0 +1,6 @@
+// ©AiiSigma
+
+module.exports = {
+  domain: "https://danzxcempe.myserverr.web.id", // Ganti sma domain panel lu dan pastiin harus *http*
+  port: "3120" // Ganti Portnoy di panel network.
+};
